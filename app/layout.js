17bc598ce679a8a2decf9f1272ece9c9d74f15f6
Josefinx <josefin.wetterberg@gmail.com>
2025-04-02 23:03:03 +0200

    continued working on landing-page components, added Footer.jsx and styling

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import Navbar from '@/components/Navbar';
+import Footer from "@/components/Footer"
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({
@@ -21,7 +22,7 @@ export default function RootLayout({ children }) {
         </header>
         <main>{children}</main>
         <footer>
-          <p>© 2025 My App</p>
+          <Footer />
         </footer>
       </body>
     </html>
