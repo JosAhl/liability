ad5be7165abd844f842bc7a6ae601983df93fa8c
Josefine <josefineahlstrand@live.se>
2025-04-11 21:02:54 +0200

    fixed reload error

--- app/layout.js
+++ app/layout.js
@@ -1,30 +1,27 @@
-'use client'
-import { Inter } from 'next/font/google';
-import Navbar from '@/components/Navbar';
-import { usePathname } from 'next/navigation';
-import Footer from "@/components/Footer"
+"use client";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
+import { usePathname } from "next/navigation";
+import Footer from "@/components/Footer";
+import "@/app/globals.css";
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({
-  subsets: ['latin'],
-  display: 'swap',
+  subsets: ["latin"],
+  display: "swap",
 });
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
-  const isAuthPage = pathname.startsWith('/login');
-  const isRegPage = pathname.startsWith('/register');
+  const isAuthPage = pathname.startsWith("/login");
+  const isRegPage = pathname.startsWith("/register");
 
   return (
     <html lang="en" className={inter.className}>
       <body>
         {/* Only show Navbar if not on auth pages */}
-        <header>
-        {!isAuthPage && !isRegPage && <Navbar />}
-        </header>
-        <main className={isAuthPage ? 'auth-page' : ''}>
-          {children}
-        </main>
+        <header>{!isAuthPage && !isRegPage && <Navbar />}</header>
+        <main className={isAuthPage ? "auth-page" : ""}>{children}</main>
         <footer>
           <Footer />
         </footer>
