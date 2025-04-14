f368cf77d19fe71423561f74478a286ad770aa95
Josefinx <josefin.wetterberg@gmail.com>
2025-04-14 22:30:44 +0200

    started working on profile-page

---
+++ app/profile/[id]/page.jsx
@@ -0,0 +1,60 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useParams } from 'next/navigation';
+import { createClient } from '@/utils/supabase/client';
+import ProfileView from '@/components/profile/ProfileView';
+import ProfileEditForm from '@/components/profile/ProfileEditForm';
+
+const supabase = createClient();
+
+export default function ProfilePage() {
+  const { id } = useParams();
+  const [profile, setProfile] = useState(null);
+  const [user, setUser] = useState(null);
+
+/*   useEffect(() => {
+    async function load() {
+      const { data: profile } = await supabase.from('profiles').select('*').eq('id', id).single();
+      setProfile(profile);
+
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+    }
+
+    load();
+  }, [id]); */
+
+  //Mock data for testing
+
+    useEffect(() => {
+
+    async function load() {
+
+        const profile = {
+
+            id: 1,
+            name: "John Doe",
+        }
+        setProfile(profil.id);
+
+        const user = {
+
+            id: 2,
+            name: "Jane Doe",
+        }
+        setUser(user.id);
+    }
+    load();
+    }
+    , [id]);
+    
+
+  if (!profile || !user) return <p>Loading...</p>;
+
+  const isOwner = profile.id === user.id;
+
+  return isOwner ? <ProfileEditForm profile={profile} /> : <ProfileView profile={profile} />;
+}
--- utils/supabase/middleware.js
+++ utils/supabase/middleware.js
@@ -44,6 +44,7 @@ export async function updateSession(request) {
     !request.nextUrl.pathname.startsWith("/login") &&
     !request.nextUrl.pathname.startsWith("/auth") &&
     request.nextUrl.pathname !== "/dashboard" && //TODO - ta bort denna sedan! 
+    request.nextUrl.pathname !== "/profile/1" && //TODO - ta bort denna sedan! 
     request.nextUrl.pathname !== "/" // Add this line to exempt the root route
   ) {
     // no user, potentially respond by redirecting the user to the login page
